814a4b01933b9321533a4faff4b1c17dfe11abe6
Attull <atul852798@gmail.com>
2022-05-31 12:29:50 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,27 @@ import { DraggableModalComponent } from './modal/draggable-modal.component';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-
+export class AppComponent {
   buttonText: string = 'Open Modal';
-  
+
   modalRef: any;
   modalOptions: NgbModalOptions = ModalConfig;
 
   constructor(private modalService: NgbModal) {}
 
   openModal() {
-    this.modalRef = this.modalService.open(DraggableModalComponent, this.modalOptions);
+    this.modalRef = this.modalService.open(
+      DraggableModalComponent,
+      this.modalOptions
+    );
     this.modalRef.componentInstance.title = 'Drag Me!';
-    this.modalRef.componentInstance.message = 'Try dragging this modal around using the modal header!';
-    this.modalRef.componentInstance.onSubmitSubject
-      .subscribe((res: boolean) => {
+    this.modalRef.componentInstance.message = 'hello!';
+    this.modalRef.componentInstance.onSubmitSubject.subscribe(
+      (res: boolean) => {
         this.buttonText = 'Open Modal Again!';
-      });
+      }
+    );
   }
 }
